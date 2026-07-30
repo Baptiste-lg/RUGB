c76fe8fd1ab2d05dfc8da714f03e364a2d571536
Baptiste-lg <baptiste.le-glaunec@epitech.eu>
2026-07-30 14:05:21 +0200

    [OPT] Eliminate redundant tile-addr computation in CGB render_bg, remove dead branch in render_window

--- rugb/src/ppu.rs
+++ rugb/src/ppu.rs
@@ -230,14 +230,6 @@ impl Ppu {
             let map_addr = tile_map_base + tile_row * 32 + tile_col;
             let tile_id = self.vram[(map_addr - 0x8000) as usize];
 
-            let tile_addr = if signed_tile_ids {
-                // 0x8800 mode: tile_id is signed, 0 maps to 0x9000
-                let signed_id = tile_id as i8 as i16;
-                (0x9000u16 as i16 + signed_id * 16 + pixel_row as i16 * 2) as u16
-            } else {
-                tile_data_base + tile_id as u16 * 16 + pixel_row * 2
-            };
-
             if self.cgb_mode {
                 let map_offset = (map_addr - 0x8000) as usize;
                 let attr = self.vram_bank1[map_offset];
@@ -246,9 +238,8 @@ impl Ppu {
                 let cgb_x_flip = attr & 0x20 != 0;
                 let cgb_y_flip = attr & 0x40 != 0;
 
-                // Recompute tile address with y-flip
                 let actual_pixel_row = if cgb_y_flip { 7 - pixel_row } else { pixel_row };
-                let actual_tile_addr = if signed_tile_ids {
+                let tile_addr = if signed_tile_ids {
                     let signed_id = tile_id as i8 as i16;
                     (0x9000u16 as i16 + signed_id * 16 + actual_pixel_row as i16 * 2) as u16
                 } else {
@@ -257,12 +248,18 @@ impl Ppu {
 
                 let actual_pixel_col = if cgb_x_flip { 7 - pixel_col } else { pixel_col };
                 let color_id =
-                    self.get_tile_pixel_banked(actual_tile_addr, actual_pixel_col, tile_bank);
+                    self.get_tile_pixel_banked(tile_addr, actual_pixel_col, tile_bank);
                 self.bg_color_ids[x as usize] = color_id;
                 let rgba =
                     Self::cgb_palette_color(&self.bg_palette_data, palette_num, color_id as usize);
                 self.set_pixel_rgba(x as usize, self.ly as usize, rgba);
             } else {
+                let tile_addr = if signed_tile_ids {
+                    let signed_id = tile_id as i8 as i16;
+                    (0x9000u16 as i16 + signed_id * 16 + pixel_row as i16 * 2) as u16
+                } else {
+                    tile_data_base + tile_id as u16 * 16 + pixel_row * 2
+                };
                 let color_id = self.get_tile_pixel(tile_addr, pixel_col);
                 self.bg_color_ids[x as usize] = color_id;
                 let shade = self.apply_palette(self.bgp, color_id);
@@ -344,9 +341,7 @@ impl Ppu {
             }
         }
 
-        if self.ly >= self.wy {
-            self.window_line += 1;
-        }
+        self.window_line += 1;
     }
 
     fn render_sprites(&mut self) {
